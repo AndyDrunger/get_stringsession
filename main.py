7854ba15a38458af25d17ce14d9223db1d71aa0d
ferzgus777 <x666031666@gmail.com>
2026-04-21 04:12:47 +0000

    fix

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
 # Твои данные
-API_ID = 38861033
-API_HASH = 'c8350d37381c29f603eef2892bb3b7f3'
+API_ID = 2040
+API_HASH = 'b18441a1ff607e10a989891a5462e627'
 
 with TelegramClient(StringSession(), API_ID, API_HASH) as client:
     print("\nТВОЯ СТРОКА СЕССИИ (СКОПИРУЙ ЕЁ ПОЛНОСТЬЮ):\n")
